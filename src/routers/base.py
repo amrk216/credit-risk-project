08741449b48523377b2a1a89707e03e0a1f8872e
Amr khaled <amrkhaledsalama98@gmail.com>
2026-03-15 08:09:08 +0200

    Refactor health and model info endpoints to use dependency injection for app settings; add data upload endpoint with file validation and error handling

--- src/routers/base.py
+++ src/routers/base.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from helpers.config import Settings, get_settings
 import os
 base_router = APIRouter(
     prefix="/api/v1",
@@ -6,19 +7,19 @@ base_router = APIRouter(
 )
 
 @base_router.get("/")
-async def Health():
-    app_name = os.getenv("APP_NAME")
-    app_version = os.getenv("APP_VERSION")
+async def Health(app_settings: Settings = Depends(get_settings)):
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
     return {"app_name": app_name,
             "version": app_version
             }
 
 
 @base_router.get("/MODEL_INFO")
-async def ModelInfo():
-    name_model = os.getenv("NAME_MODEL")
-    version_model = os.getenv("version_model")
-    roc_auc = os.getenv("ROC_AUC")
+async def ModelInfo(app_settings: Settings = Depends(get_settings)):
+    name_model = app_settings.NAME_MODEL
+    version_model = app_settings.VERSION_MODEL
+    roc_auc = app_settings.ROC_AUC
     return {"model_name": name_model,
             "model_version": version_model,
             "roc_auc": roc_auc
